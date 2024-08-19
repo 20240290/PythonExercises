7a775a36a7f7be1f7099c4735ec7b9a789ba45bc
Joseph Patambag <jpatambag@gmail.com>
2024-08-19 12:18:37 -0300

    start chapter 8

---
+++ Chapter8/Chap8Module.py
@@ -0,0 +1,18 @@
+# Joseph Patambag
+# August 19, 2024
+
+#Function declarations.
+
+def display_message():
+    """
+    Print a string message on what we learn on this chapter in the python crash course.
+    
+    Parameters
+    ----------
+    none
+        
+    Returns
+    -------
+    none
+    """
+    print("Chapter 8 introduces another way of creating functions and modules to incorporate into the code. It makes code functionality neat and precise. /n")
