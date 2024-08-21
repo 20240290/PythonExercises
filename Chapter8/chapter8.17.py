fdde0a96a8920fb87fbf8a21a3d6011b8a596c4c
20240290 <joseph20240290@gmail.com>
2024-08-21 11:24:26 -0300

    exercise 8

--- Chapter8/chapter8.17.py
+++ Chapter8/chapter8.17.py
@@ -2,6 +2,34 @@
 # August 19, 2024
 
 #Module import
-import Chap8Module as module
 
 # 8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section.
+
+def make_car(manufacturer: str, model: str, **car_props):
+    """
+    Method to show information of a specific car.
+
+    Parameters
+    ----------
+    manufacturer: string
+        Car manufacturer.
+    model: string
+        Car model.
+    car_props: variable
+        Variable parameter which will add more information of the car.
+
+    Returns
+    -------
+    user_info: dictionary
+        Car information.
+
+    """
+    #car variable that holds the manufacturer and model
+    car = {
+        'manufacturer': manufacturer,
+        'model': model
+    }
+    #iterate car props and add it in the car dictionary
+    for key,value in car_props.items():
+        car[key] = value
+    return car  
